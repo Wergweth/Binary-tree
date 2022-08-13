62a09794bfd5f8345bfd29420cf118c20c0f0eb7
Oleg <Wergweth@rambler.ru>
2022-08-13 03:39:58 +0300

    Binary tree structure

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+type node struct {
+	value int
+	left  *node
+	right *node
+}
 
 func main() {
-	fmt.Println("test")
+	var slice = []int{2, 4, 8, 5, 3, 14, 36, 25, 42, 37, 59, 47, 64, 58, 72, 65, 80, 73, 91, 110}
 }
