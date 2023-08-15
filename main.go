2487c7e5526af263b67f2f915bafcb84d8cc6565
Oleg <Wergweth@rambler.ru>
2023-08-15 19:19:47 +0300

    simple tree with add, remove and output functions

--- main.go
+++ main.go
@@ -1,11 +1,100 @@
 package main
 
-type node struct {
+import "fmt"
+
+type Node struct {
 	value int
-	left  *node
-	right *node
+	left  *Node
+	right *Node
+}
+
+type BinaryTree struct {
+	root *Node
+}
+
+func (b *BinaryTree) insertNode(value int) {
+	if b.root == nil {
+		b.root = &Node{value: value}
+	} else {
+		b.root.insertNode(value)
+	}
+}
+
+func (n *Node) insertNode(value int) {
+	if value < n.value {
+		if n.left == nil {
+			n.left = &Node{value: value}
+		} else {
+			n.left.insertNode(value)
+		}
+	} else {
+		if n.right == nil {
+			n.right = &Node{value: value}
+		} else {
+			n.right.insertNode(value)
+		}
+	}
+}
+
+func (b *BinaryTree) removeNode(value int) {
+	if b.root != nil {
+		b.root = b.root.removeNode(value)
+	}
+}
+
+func (n *Node) removeNode(value int) *Node {
+	if n == nil {
+		return nil
+	}
+	if value == n.value {
+		if n.left == nil && n.right == nil {
+			return nil
+		}
+		if n.left == nil {
+			return n.right
+		}
+		if n.right == nil {
+			return n.left
+		}
+		tempNode := n.right
+		for tempNode.left != nil {
+			tempNode = tempNode.left
+		}
+		n.value = tempNode.value
+		n.right = n.right.removeNode(tempNode.value)
+	} else if value < n.value {
+		n.left = n.left.removeNode(value)
+	} else {
+		n.right = n.right.removeNode(value)
+	}
+	return n
+}
+
+func (b *BinaryTree) printTree() {
+	if b.root != nil {
+		b.root.printTree(0)
+	}
+}
+
+func (n *Node) printTree(level int) {
+	if n != nil {
+		n.right.printTree(level + 1)
+		for i := 0; i < level; i++ {
+			fmt.Print("   ")
+		}
+		fmt.Printf("%d\n", n.value)
+		n.left.printTree(level + 1)
+	}
 }
 
 func main() {
-	var slice = []int{2, 4, 8, 5, 3, 14, 36, 25, 42, 37, 59, 47, 64, 58, 72, 65, 80, 73, 91, 110}
+	tree := BinaryTree{}
+	tree.insertNode(1)
+	tree.insertNode(2)
+	tree.insertNode(3)
+	tree.insertNode(5)
+	tree.insertNode(4)
+	tree.printTree()
+	tree.removeNode(3)
+	tree.printTree()
 }
